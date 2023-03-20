e8815d1c2bcc3c67115bf35553d92eca62389b84
Jadyn Gomez <jadyngg19@gmail.com>
2023-03-19 21:53:04 -0400

    Almost finished

--- script.js
+++ script.js
@@ -1,108 +1,163 @@
-// Set variables for the quiz
-var currentQuestion = 0;
-var score = 0;
-var timeLeft = 75;
-var timerInterval;
-var quizContainer = document.querySelector(".quizContainer");
-var questionContainer = document.getElementById("question");
-var choicesContainer = document.getElementById("choiceList");
-var messageContainer = document.getElementById("quizMessage");
-var resultContainer = document.getElementById("result");
-
-// Set questions for the quiz
-var questions = [
-{
-question: "What does HTML stand for?",
-choices: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Text Markup Language", "None of the above"],
-correctAnswer: 2
-},
-{
-question: "What does CSS stand for?",
-choices: ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "None of the above"],
-correctAnswer: 0
-},
-{
-question: "What is the correct syntax for creating a function in JavaScript?",
-choices: ["function = myFunction()", "function:myFunction()", "function myFunction()", "None of the above"],
-correctAnswer: 2
-},
-{
-question: "What symbol is used to access jQuery?",
-choices: ["%", "#", "$", "&"],
-correctAnswer: 2
-},
-{
-question: "What is the correct syntax for commenting in JavaScript?",
-choices: ["<!--This is a comment-->", "//This is a comment", "/This is a comment/", "None of the above"],
-correctAnswer: 1
-}
+// Quiz questions and answers
+const questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Paris", "London", "Berlin"],
+        correctAnswer: 0
+    },
+    {
+        question: "What is the highest mountain in the world?",
+        choices: ["K2", "Mount Everest", "Makalu"],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the currency of Japan?",
+        choices: ["Yen", "Dollar", "Euro"],
+        correctAnswer: 0
+    },
+    {
+        question: "What is the largest country in the world?",
+        choices: ["China", "Russia", "United States"],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the capital of Brazil?",
+        choices: ["Rio de Janeiro", "Brasilia", "Sao Paulo"],
+        correctAnswer: 1
+    }
 ];
 
-// Function to begin the quiz
-function beginQuiz() {
-document.getElementById("welcome").style.display = "none";
-document.getElementById("theTime").style.display = "block";
-showQuestion();
-startTimer();
-}
+// Variables
+let currentQuestion = 0;
+let score = 0;
+let timeLeft = 60;
+let timer;
+const quizContainer = document.querySelector(".quizContainer");
+const questionEl = document.getElementById("question");
+const choiceListEl = document.getElementById("choiceList");
+const messageEl = document.getElementById("quizMessage");
+const resultEl = document.getElementById("result");
+const startButtonEl = document.getElementById("startButton");
+const nextButtonEl = document.getElementById("nextButton");
 
-// Function to show the current question
-function showQuestion() {
-questionContainer.innerHTML = questions[currentQuestion].question;
-choicesContainer.innerHTML = "";
-for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
-var choice = questions[currentQuestion].choices[i];
-var li = document.createElement("li");
-li.setAttribute("class", "choice");
-li.setAttribute("data-index", i);
-li.innerHTML = choice;
-li.addEventListener("click", checkAnswer);
-choicesContainer.appendChild(li);
-}
-}
-
-// Function to check the selected answer
-function checkAnswer(event) {
-var selectedChoice = event.target;
-var selectedIndex = selectedChoice.getAttribute("data-index");
-if (selectedIndex == questions[currentQuestion].correctAnswer) {
-score++;
-messageContainer.innerHTML = "Correct!";
-} else {
-timeLeft -= 10;
-messageContainer.innerHTML = "Incorrect.";
-}
-if (currentQuestion == questions.length - 1) {
-showResult();
-} else {
-currentQuestion++;
-showQuestion();
-}
-}
-
-// Function to show the quiz result
-function showResult() {
-clearInterval(timerInterval);
-quizContainer.style.display = "none";
-resultContainer.style.display = "block";
-var resultText = "You scored " + score + " out of " + questions.length + " questions!";
-resultContainer.innerHTML = resultText;
-}
-
-// Function to start the timer
+// Timer function
 function startTimer() {
-timerInterval = setInterval(function() {
-timeLeft--;
-document.getElementById("timer").innerHTML = timeLeft;
-if (timeLeft <= 0) {
-clearInterval(timerInterval);
-showResult();
-}
-}, 1000);
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timer").textContent = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endQuiz();
+        }
+    }, 1000);
 }
 
-// Event listener for the next question button
-document.getElementById("nextButton").addEventListener("click", function() {
-currentQuestion++;
-showQuestion();
-});
+// Begin quiz function
+function beginQuiz() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("theTime").style.display = "block";
+    startButtonEl.style.display = "none";
+    nextButtonEl.style.display = "none";
+    showQuestion();
+    startTimer();
+}
+
+// Show question function
+function showQuestion() {
+    questionEl.innerHTML = questions[currentQuestion].question;
+    choiceListEl.innerHTML = "";
+    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+        const choice = questions[currentQuestion].choices[i];
+        const li = document.createElement("li");
+        li.innerHTML = `<button onclick='checkAnswer(${i})'>${choice}</button>`;
+        choiceListEl.appendChild(li);
+    }
+}
+
+// Check answer function
+function checkAnswer(answer) {
+    if (answer === questions[currentQuestion].correctAnswer) {
+        score++;
+        messageEl.textContent = "Correct!";
+    } else {
+        timeLeft -= 10;
+        messageEl.textContent = "Wrong! -10 seconds";
+    }
+    messageEl.style.display = "block";
+    nextButtonEl.style.display = "block";
+    startButtonEl.style.display = "none";
+}
+
+// Next question function
+function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion === questions.length) {
+        endQuiz();
+    } else {
+        showQuestion();
+        messageEl.style.display = "none";
+        nextButtonEl.style.display = "none";
+        startButtonEl.style.display = "block";
+    }
+}
+
+// End quiz function
+function endQuiz() {
+    clearInterval(timer);
+    quizContainer.innerHTML = `
+        <h2>Quiz Complete</h2>
+        <p>Your score is ${score}.</p>
+        <button onclick="showLeaderboard()">Show Leaderboard</button>
+    `;
+}
+
+// Leaderboard function
+function showLeaderboard() {
+    const highScores = localStorage.getItem("highScores")
+      ? JSON.parse(localStorage.getItem("highScores"))
+      : [];
+  
+    highScores.push(score);
+  
+    highScores.sort((a, b) => b - a);
+  
+    while (highScores.length > 5) {
+      highScores.pop();
+    }
+  
+    const initials = prompt("Enter your initials:");
+  
+    const entry = {
+      initials,
+      score
+    };
+  
+    highScores.push(entry);
+  
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+    const leaderboardHTML = `
+      <h2>High Scores</h2>
+      <ol>
+        ${highScores
+          .map(
+            (entry) =>
+              `<li>${entry.initials}: ${entry.score}</li>`
+          )
+          .join("")}
+      </ol>
+      <button onclick="location.reload()">Try Again</button>
+    `;
+  
+    quizContainer.innerHTML = leaderboardHTML;
+  }
+  
+  
+
+// Initialize quiz
+questionEl.innerHTML = "Press the start button to begin the quiz.";
+messageEl.style.display = "none";
+resultEl.style.display = "none";
+nextButtonEl.style.display = "none";
+startButtonEl.addEventListener("click", beginQuiz);
+nextButtonEl.addEventListener("click", nextQuestion);        
